d37c6c9e6e5a0609b4797533c8068e915ad05f8d
Ender Invader <derek.scandrett@cox.net>
2020-07-06 09:06:51 -0700

    Added Destiny Info Collection

---
+++ Destiny/scripts.js
@@ -0,0 +1,18 @@
+// Create a request variable and assign a new XMLHttpRequest object to it.
+var request = new XMLHttpRequest()
+
+// Open a new connection, using the GET request on the URL endpoint
+request.open('GET', 'https://api.vendorengrams.xyz/getVendorDrops')
+
+request.onload = function () {
+  // Begin accessing JSON data here
+  var data = JSON.parse(this.response)
+
+  data.forEach((vendor) => {
+    // Log each vendor's name
+    console.log(vendor.shorthand)
+  })
+}
+
+// Send request
+request.send()
